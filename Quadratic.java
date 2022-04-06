e8d51c7e61ee969c1e2de3cb01d1ea686f2ba337
William Michael Kanar <78453917+TheWilbo@users.noreply.github.com>
2022-04-06 09:23:07 -0400

    Added circle solver

--- Quadratic.java
+++ Quadratic.java
@@ -5,12 +5,19 @@ class Quadratic{
         double c = 18;
         double first = 3;
         double second = 4;
-        System.out.println("Quadratic:"+calc(a,b,c,true));
-        System.out.println("Quadratic:"+calc(a,b,c,false));
-        System.out.println("Pythag:"+pythag(first,second,true));
+        double u = 1;
+        double v = 2;
+        double x = 3;
+        double y = 4;
+        double k = 5;
+        double h = 6;
+        System.out.println("Quadratic: "+calc(a,b,c,true));
+        System.out.println("Quadratic: "+calc(a,b,c,false));
+        System.out.println("Pythag: "+pythag(first,second,true));
         System.out.println(pythag(first,second,false));
-        System.out.println("Sum of cubes:("+a+"^3)+("+b+"^3)=("+a+"+"+b+")(("+a+"^2)-"+a+"*"+b+"+("+b+"^2))");
-        System.out.println("Difference of cubes:("+a+"^3)-("+b+"^3)=("+a+"-"+b+")(("+a+"^2)+"+a+"*"+b+"+("+b+"^2))");
+        System.out.println("Sum of cubes:("+u+"^3)+("+v+"^3)=("+u+"+"+v+")(("+u+"^2)-"+u+"*"+v+"+("+v+"^2))");
+        System.out.println("Difference of cubes:("+u+"^3)-("+v+"^3)=("+u+"-"+v+")(("+u+"^2)+"+u+"*"+v+"+("+v+"^2))");
+        System.out.println("Radius of the circle: "+circle(x,y,k,h));
     }
     public static double calc(double a, double b, double c,boolean runPositive){
         double returnStatement;
@@ -33,5 +40,9 @@ class Quadratic{
         returnStatement = Math.sqrt(Math.pow(first,2) - Math.pow(second,2));
       }
       return returnStatement;
+    }
+    public static double circle(double x, double y, double k, double h){
+      double r = Math.sqrt((Math.pow(x-h,2))+(Math.pow(y-k,2)));
+      return r;
     }
 }
