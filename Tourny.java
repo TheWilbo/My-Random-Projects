e00b0990fe3451833c88b4ce7e50f71a1e9a5700
William Michael Kanar <78453917+TheWilbo@users.noreply.github.com>
2022-04-19 10:26:47 -0400

    Made it use a double

--- Tourny.java
+++ Tourny.java
@@ -1,7 +1,7 @@
 class Tourny{
    public static void main(String args[]){
        long i =0L;
-       for (long peopleLeft = 7900000000L; peopleLeft > 1; peopleLeft /= 2){
+       for (double peopleLeft = 7900000000; peopleLeft > 1; peopleLeft /= 2){
            ++i;
            System.out.println(i+":"+peopleLeft);
        }
