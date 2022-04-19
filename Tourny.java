7f340d1bd968bef8f0c28f3ab82ee9c41df03fa3
William Michael Kanar <78453917+TheWilbo@users.noreply.github.com>
2022-04-19 13:51:09 -0400

    Changed

--- Tourny.java
+++ Tourny.java
@@ -1,7 +1,7 @@
 class Tourny{
    public static void main(String args[]){
        long i =0L;
-       for (double peopleLeft = 7900000000; peopleLeft > 1; peopleLeft /= 2){
+       for (long peopleLeft = 7900000000L; peopleLeft > 1; peopleLeft = (peopleLeft/2)+peopleLeft%2){
            ++i;
            System.out.println(i+":"+peopleLeft);
        }
