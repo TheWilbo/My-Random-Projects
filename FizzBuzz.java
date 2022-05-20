42ef8d362078ab3467b598555575a1d2a61abeed
The_Wilbo <kanarwm@outlook.com>
2022-05-19 20:46:18 -0400

    Kinda fixed it

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -1,17 +1,19 @@
 public class FizzBuzz{
     public static void main(String args[]){
         String out;
-        for(long i=1; i<999999; ++i){
-            if (i%3==0 && i%5==0){
-                out = "fizzbuzz";
-            }else if (i%5==0){
-                out = "buzz";
-            }else if (i%3==0){
+        for(long i=1; i<=9223372036854775807L; ++i){
+            out = "";
+            if (i%3==0){
                 out = "fizz";
-            } else{
-                out = "";
             }
-            System.out.println(i+":"+out);
+            if (i%5==0){
+                out = out+"buzz";
+            } 
+            if (out == ""){
+                System.out.println(i);
+                continue;
+            }
+            System.out.println(out);
         }
     }
 }
