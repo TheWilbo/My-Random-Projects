6d67fb7dee38f16802afe03cfe0589c66ab3d0a9
The_Wilbo <kanarwm@outlook.com>
2022-05-23 18:01:38 -0400

    It only does the important stuff now

--- Bean.java
+++ Bean.java
@@ -1,10 +1,7 @@
 public class Bean{
     public static void main(String[] args) {
-        final double pi = Math.PI*(1);
-        String word = "Bean", ligma = "Ligma", ballz = "Balls", LigmaBallz = ligma + ballz; 
-        int whole = 9/2, remainder = 9%2, both = whole |= remainder, stringCount = 0;
-        //System.out.println(word + pi + LigmaBallz + stringCount + txt.length());
         while (true){
-        System.out.println("Colter is gay");}
+            System.out.println("Colter is gay");
         }
+    }
 }
