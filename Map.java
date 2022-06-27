f2b0503371e29d53fac0783048b84255ae13b00b
The_Wilbo <kanarwm@outlook.com>
2022-06-26 22:59:59 -0400

    Added GUI to Map.java

--- Map.java
+++ Map.java
@@ -1,7 +1,21 @@
+import java.awt.BorderLayout;
 import java.net.URI;
+
+import javax.swing.BorderFactory;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class Map {
     public static void main(String[] args){
+        JFrame frame = new JFrame();
+        JPanel panel = new JPanel();
+        panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
+        //panel.setLayout(new GridLayout(0,1));
+        frame.add(panel, BorderLayout.CENTER);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("ooooo");
+        frame.pack();
+        frame.setVisible(true);
         try {
    
         URI uri= new URI("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
