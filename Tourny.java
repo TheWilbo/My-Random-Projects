a373bdd1ffce39f9e34520064a26839df2c42a03
The_Wilbo <kanarwm@outlook.com>
2022-07-08 10:17:09 -0400

    0xBEEF is a funny number.

--- Tourny.java
+++ Tourny.java
@@ -1,9 +1,10 @@
 class Tourny{
    public static void main(String args[]){
        long i =0L;
-       for (long peopleLeft = 7900000000L; peopleLeft > 1; peopleLeft = (peopleLeft/2)+peopleLeft%2){
+       for (long peopleLeft = 0xbeef; peopleLeft > 1; peopleLeft = (peopleLeft/2)+peopleLeft%2){
            ++i;
            System.out.println(i+":"+peopleLeft);
        }
    } 
 }
+//7900000000L
