564c51878ced10a034977048795812ffc8821d75
The_Wilbo <kanarwm@outlook.com>
2022-07-24 18:04:44 -0400

    More hypotenuse

--- Pythag.java
+++ Pythag.java
@@ -1,7 +1,7 @@
 public class Pythag {
-    public static double pythag(double first, double second, boolean solveForHypotnuse){
+    public static double pythag(double first, double second, boolean solveForHypotenuse){
         double returnStatement;
-        if (solveForHypotnuse){
+        if (solveForHypotenuse){
             returnStatement = Math.sqrt(Math.pow(first,2) + Math.pow(second,2));
         } else {
             returnStatement = Math.sqrt(Math.pow(first,2) - Math.pow(second,2));
